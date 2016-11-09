7fc0ae301584824d5cca2a2c8657d6dd706acae1
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-11-09 13:53:06 +0200

    update interface

--- android/eu.sigrlami/Sim.java
+++ android/eu.sigrlami/Sim.java
@@ -1,5 +1,33 @@
 package eu.sigrlami.sim;
 
-public class RNSim extends {
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+
+public class RNDeviceInfo implements ReactPackage {
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+    modules.add(new RNSimModule(reactContext));
+    return modules;
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+  	return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+  	return Collections.emptyList();
+  }
 
 }
