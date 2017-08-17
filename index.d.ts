3c5f65b0517657e4d0b085e0f19691664c33e116
Paulo Cesar <email@pocesar.e4ward.com>
2017-08-17 07:20:42 -0300

    gitignore doesnt accept relative paths?

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import { IRNSimInfo } from './RNSimInfo';
-declare const _default: {
-    getSimInfo(): IRNSimInfo;
-    getTelephoneNumber(): string;
-    getCarrierName(): string;
-    getCountryCode(): string;
-};
-export default _default;
