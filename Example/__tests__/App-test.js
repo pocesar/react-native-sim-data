3ebb0b7f8b2625da7e9ab0691fbbbde118225bce
Paulo Cesar <email@pocesar.e4ward.com>
2019-07-21 15:49:48 -0300

    add dummy debug keystore

--- Example/__tests__/App-test.js
+++ Example/__tests__/App-test.js
@@ -4,7 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+import Index from '../index';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
--- Example/android/app/src/main/java/com/example/MainApplication.java
+++ Example/android/app/src/main/java/com/example/MainApplication.java
@@ -9,10 +9,8 @@ import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -32,6 +30,10 @@ public class MainApplication extends Application implements ReactApplication {
       return packages;
     }
   };
+
+  protected String getJSMainModuleName() {
+    return "index";
+  }
 
   @Override
   public ReactNativeHost getReactNativeHost() {
