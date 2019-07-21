3e54079e31afe16feef02fa5b1ea66336514e8e6
Paulo Cesar <email@pocesar.e4ward.com>
2019-07-21 15:59:45 -0300

    travis licenses

--- prepare.sh
+++ prepare.sh
@@ -6,5 +6,5 @@ npm link
 cd Example
 npm install
 npm link react-native-sim-data
-react-native link react-native-sim-data
+#react-native link react-native-sim-data
 cd android && chmod +x gradlew && ./gradlew assembleDebug && ./gradlew assembleRelease
